4bcedbafcda8534207547f55479c5b5aca71bc49
Daryl Chong <darylchonggz@gmail.com>
2022-11-15 17:48:40 +0800

    updated listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,22 +1,31 @@
 class ListingsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def show
+    @list = Listing.find(params[:id])
   end
 
   def new
+    @list = Listing.new
   end
 
   def create
   end
 
   def edit
+    @list = Listing.new
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit()
   end
 end
