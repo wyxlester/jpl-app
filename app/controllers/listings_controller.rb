76371b256b813292b5739bbf49943a906eb77e45
collin chua <collin-chua@hotmail.com>
2022-11-16 15:18:04 +0800

    created create method in listing controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,5 @@
 class ListingsController < ApplicationController
+  before_action :all_user
   def index
     @listing = Listing.all
   end
@@ -12,8 +13,9 @@ class ListingsController < ApplicationController
   end
 
   def create
-    @user = User.find(2)
-    raise
+    listing = Listing.new(listing_params)
+    listing.user = @user.sample
+    listing.save
   end
 
   def edit
@@ -33,5 +35,10 @@ class ListingsController < ApplicationController
 
   def listing_params
     params.require(:listing).permit(:item_name, :item_description, :price, :user)
+  end
+
+  def all_user
+    # delete this when we can have users
+    @user = User.all
   end
 end
