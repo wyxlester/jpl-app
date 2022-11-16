2cfe0e0eb2a575b20636e7f8fce85fe83516d552
collin chua <collin-chua@hotmail.com>
2022-11-16 14:44:22 +0800

    Test file

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,5 @@
 class ListingsController < ApplicationController
+  before_action :all_user
   def index
     @listing = Listing.all
   end
@@ -12,6 +13,10 @@ class ListingsController < ApplicationController
   end
 
   def create
+    listing = Listing.new(listing_params)
+    listing.user = @user.sample
+    raise
+    listing.save
   end
 
   def edit
@@ -30,6 +35,10 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
-    params.require(:listing).permit()
+    params.require(:listing).permit(:item_name, :item_description, :price, :user)
+  end
+
+  def all_user # delete this when we can have users
+    @user = User.all
   end
 end
